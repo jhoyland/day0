dcff26b65e72e655407d6a9e1769722e6db77946
James Hoyland <hoyland.jd@gmail.com>
2021-09-07 09:45:51 -0700

    Created project day0

---
+++ helloworld.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	int x; // Declaration
+	x = 7; // Initialization
+
+	int y = 5; // Both together
+
+	int z;
+
+	z = x+y;
+
+	printf("Hello world! %d ",z);
+
+	return 0;
+}
